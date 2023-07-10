57d98827c6822873960cffa2e6afffecccf8ab59
lava114514 <2697345722@qq.com>
2023-07-10 11:31:57 +0800

    update

--- src/main/java/enderlava/tpj/Tpj.java
+++ src/main/java/enderlava/tpj/Tpj.java
@@ -1,17 +1,20 @@
 package enderlava.tpj;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
-
 public class Tpj extends JavaPlugin {
 
     private Map<Player, Player> teleportRequests;
-
+    private Map<Player, Player> teleportAccepts = new HashMap<>();
     @Override
     public void onEnable() {
         teleportRequests = new HashMap<>();
@@ -28,12 +31,12 @@ public class Tpj extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("tpj")) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage("&b该命令只能由玩家执行！");
+                sender.sendMessage(ChatColor.AQUA + "该命令只能由玩家执行！");
                 return true;
             }
 
             if (args.length == 0) {
-                sender.sendMessage("&b用法: /tpj <玩家名>");
+                sender.sendMessage(ChatColor.AQUA + "用法: /tpj <玩家名>");
                 return true;
             }
 
@@ -41,20 +44,20 @@ public class Tpj extends JavaPlugin {
             Player target = getServer().getPlayer(args[0]);
 
             if (target == null) {
-                sender.sendMessage("&b找不到指定的玩家！");
+                sender.sendMessage(ChatColor.AQUA + "找不到指定的玩家！");
                 return true;
             }
 
             teleportRequests.put(target, player);
-            sender.sendMessage("&b传送请求已发送给玩家 " + target.getName() + "，&b请等待对方的回应！");
-            target.sendMessage("&b玩家 " + player.getName() + " &b请求传送到你的位置。同意请使用命令: /tpjaccept");
+            sender.sendMessage(ChatColor.AQUA + "传送请求已发送给玩家 " + target.getName() + "，请等待对方的回应！");
+            target.sendMessage(ChatColor.AQUA + "玩家 " + player.getName() + " 请求传送到你的位置。同意请使用命令: /tpjaccept");
 
             return true;
         }
 
         if (label.equalsIgnoreCase("tpjaccept")) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage("&b该命令只能由玩家执行！");
+                sender.sendMessage(ChatColor.AQUA + "该命令只能由玩家执行！");
                 return true;
             }
 
@@ -62,20 +65,38 @@ public class Tpj extends JavaPlugin {
             Player requester = teleportRequests.get(target);
 
             if (requester == null) {
-                sender.sendMessage("&b没有待处理的传送请求！");
+                sender.sendMessage(ChatColor.AQUA + "没有待处理的传送请求！");
                 return true;
             }
 
-            requester.teleport(target.getLocation());
-            requester.sendMessage("&b你已经传送到了玩家 " + target.getName() + " &b的位置！");
-            teleportRequests.remove(target);
-            return true;
-        }
-        if (label.equalsIgnoreCase("tpjaccept"))
-        {
-            sender.sendMessage("&bJ志坤tp");
-            sender.sendMessage("&bBy Enerlava");
-            sender.sendMessage("&bv1.0");
+            teleportAccepts.put(requester, target);
+            requester.sendMessage(ChatColor.GREEN + "你的传送请求已被玩家 " + target.getName() + " 接受！请等待传送完成...");
+            target.sendMessage(ChatColor.GREEN + "你已接受玩家 " + requester.getName() + " 的传送请求，正在传送中...");
+            requester.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 3 * 20, 5));
+            // 使用 BukkitRunnable 创建一个延迟任务
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (teleportAccepts.containsKey(requester) && teleportAccepts.get(requester) == target) {
+                        teleportAccepts.remove(requester);
+                        requester.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 3 * 20, 5));
+                        requester.teleport(target.getLocation());
+                        requester.sendMessage(ChatColor.GREEN + "你已经传送到了玩家 " + target.getName() + " 的位置！");
+                    }
+                }
+            };
+
+            if (label.equalsIgnoreCase("tpjhelp")) {
+                sender.sendMessage(ChatColor.AQUA + "J志坤tp");
+                sender.sendMessage(ChatColor.RED + "By Enderlava");
+                sender.sendMessage(ChatColor.YELLOW + "v1.0");
+                sender.sendMessage(ChatColor.YELLOW + "我劝你不要不识好歹");
+                sender.sendMessage(ChatColor.AQUA + "=== TPJ 插件帮助 ===");
+                sender.sendMessage(ChatColor.YELLOW + "/tpj <玩家名> - 发送传送请求给指定玩家");
+                sender.sendMessage(ChatColor.YELLOW + "/tpjaccept - 接受传送请求");
+                return true;
+            }
+            return false;
         }
         return false;
     }
